d362a0c5bda07741bbc4a2a899b54dac65d53f8a
Thiago Barros <t.b.lima18@gmail.com>
2022-03-01 10:05:33 -0300

    Added the necessary dependencies for the Google Maps to work

--- app/src/main/java/com/example/mapsbootcamp/MapsActivity.kt
+++ app/src/main/java/com/example/mapsbootcamp/MapsActivity.kt
@@ -1,20 +1,22 @@
 package com.example.mapsbootcamp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mapsbootcamp.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.mapsbootcamp.databinding.ActivityMapsBinding
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,5 +46,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        mMap.setOnMarkerClickListener(this)
+        mMap.uiSettings.isZoomControlsEnabled = true
     }
+
+    override fun onMarkerClick(p0: Marker) = false
 }
